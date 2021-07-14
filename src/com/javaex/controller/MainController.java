f546f643569faa26b785b52d24f75841fe6db429
wonho <wonho951@gmail.com>
2021-07-14 11:01:09 +0900

    2021.07.14 수업내용 mysite

--- src/com/javaex/controller/MainController.java
+++ src/com/javaex/controller/MainController.java
@@ -7,15 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javaex.util.WebUtil;
 
-@WebServlet("/MainController")
+
+@WebServlet("/main")
 public class MainController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;	//아직은 몰라도 되는거. 중복 안되게 시리얼번호 주는거 라는 느낌으로 말씀해주셨음.
        
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("[MainController]");
 		
+		//포워드
+		WebUtil.forword(request, response, "/WEB-INF/views/main/index.jsp");
 		
 	}
 
